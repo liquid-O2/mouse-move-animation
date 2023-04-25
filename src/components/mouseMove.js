7eec994413c2ffd2241982e24d186846d72d62ba
arunava(liquido2) <this.is.arunava.b@gmail.com>
2023-04-25 15:22:20 +0530

    Change to mouseMove

--- src/components/mouseMove.js
+++ src/components/mouseMove.js
@@ -1,6 +1,7 @@
-import React, { useState, useRef } from "react"
+import React, { useRef } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { useEffect } from "react"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 const MouseMoveAnimation = () => {
   const data = useStaticQuery(graphql`
@@ -9,9 +10,7 @@ const MouseMoveAnimation = () => {
         edges {
           node {
             childImageSharp {
-              fixed(quality: 30) {
-                src
-              }
+              gatsbyImageData(quality: 20)
             }
           }
         }
@@ -20,32 +19,40 @@ const MouseMoveAnimation = () => {
   `)
 
   const images = data.allFile.edges.map(file => {
-    return file.node.childImageSharp.fixed.src
+    return file.node.childImageSharp.gatsbyImageData
   })
 
-  const [imageSource, setImageSource] = useState(null)
   const index = useRef(0)
   const initialMousePosition = useRef({ x: 0, y: 0 })
-  const mouseMovedThreshold = 100
+  const mouseMovedThreshold = 250
 
   useEffect(() => {
     const container = document.querySelector(".hero")
-    function changeImageSourceOnMouseMove(e) {
+    const imageWrappers = document.querySelectorAll(".image-wrapper")
+
+    function unhideImagesOnMouseMove(e) {
       const currentMousePosition = { x: e.clientX, y: e.clientY }
       const distanceMoved = Math.sqrt(Math.pow(currentMousePosition.x - initialMousePosition.current.x, 2) + Math.pow(currentMousePosition.y - initialMousePosition.current.y, 2))
-
       if (distanceMoved > mouseMovedThreshold) {
-        setImageSource(images[index.current])
+        const prevIndex = index.current
         index.current === images.length - 1 ? (index.current = 0) : index.current++
         initialMousePosition.current = currentMousePosition
+        imageWrappers[prevIndex].classList.add("hidden")
+        imageWrappers[index.current].classList.remove("hidden")
       }
     }
-    container.addEventListener("mousemove", e => changeImageSourceOnMouseMove(e))
+
+    container.addEventListener("mousemove", e => unhideImagesOnMouseMove(e))
   }, [images])
 
   return (
-    <div className='image-container flex justify-center align-center w-100 pos-abs m0'>
-      <div className='image-wrapper overflow w-100 h-100'>{imageSource && <img src={imageSource} alt='' />}</div>
+    <div className='image-container overflow w-100 h-100 pos-rel'>
+      {images &&
+        images.map((source, index) => (
+          <div key={index} index={index} className='image-wrapper hidden flex justify-center align-center'>
+            <GatsbyImage image={source} className='image' alt='' quality={20} />
+          </div>
+        ))}
     </div>
   )
 }
